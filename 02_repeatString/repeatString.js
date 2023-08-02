7ff8b1d86d9b1ab54c1134e0dc54577db62c4a99
Amirhossein Nasr <amir1381h@gmail.com>
2023-08-01 22:09:08 -0400

    Finished exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    //Takes care of the case when num is a negative number by returning ERROR
+    if (num < 0) {
+        return "ERROR";
+    }
+    
+    //The final string that we will be returning
+    let finalString = "";
 
+    //Loop used to create the string that is asked
+    for (let i = 0; i < num; i++) {
+        finalString += string;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
