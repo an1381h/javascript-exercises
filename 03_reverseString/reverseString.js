5bb66410c1931770a75ac02faa6d0bb80ce33bbb
Amirhossein Nasr <amir1381h@gmail.com>
2023-08-01 22:20:33 -0400

    Finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    //returns the reverse word
+    return word.split('').reverse().join('');
 };
 
 // Do not edit below this line
