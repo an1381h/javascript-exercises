4eb9df13a28358941a2030be9b729ea93a1ffcb7
Amirhossein Nasr <amir1381h@gmail.com>
2023-08-02 04:51:35 -0400

    Finished exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //Checks when year is divisible by 4 and 100 and 400
+    if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+        return true;
 
+    //Checks when year is divisible by 4 and 100
+    } else if (year % 4 == 0 && year % 100 == 0) {
+        return false;
+
+    //Checks when year is only divisible by 4
+    } else if (year % 4 == 0) {
+        return true;
+    }
+
+    //Other general cases
+    return false;
 };
 
 // Do not edit below this line
