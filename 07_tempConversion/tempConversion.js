374f7ca75c4376c7872e53d7cec27ff29599374f
Amirhossein Nasr <amir1381h@gmail.com>
2023-08-02 05:03:31 -0400

    Finished exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+//Fahrenheit to Celsius formula: 5/9(temp - 32)
+const convertToCelsius = function(temp) {
+  return parseFloat((5/9 * (temp - 32)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+//Celsius to Fahrenheit formual: (temp * 9/5) + 32
+const convertToFahrenheit = function(temp) {
+  return parseFloat(((temp * 9/5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
