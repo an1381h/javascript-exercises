f0782c0e4d97269e6967c3c8af4a49ad57d637be
Amirhossein Nasr <amir1381h@gmail.com>
2023-08-03 18:39:28 -0400

    Finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    //Case of negative input, 0, or 1
+    switch(true) {
+        case num < 0:
+            return "OOPS";
+        
+        case num === 0:
+            return 0;
 
+        case num === 1:
+            return 1;
+    }
+    
+    let firstPrevious = 1;
+    let secondPrevious = 1;
+    let current = 1;
+    //Setting up a loop, starting from the 2nd sequence
+    for (let i = 2; i < num; i++) {
+        current = firstPrevious + secondPrevious;
+        firstPrevious = secondPrevious;
+        secondPrevious = current;
+    }
+
+    return current;
+
+    
 };
 
 // Do not edit below this line
