2cd4328f0d0aaa0d890ba39151eee1a1b61071c1
Amirhossein Nasr <amir1381h@gmail.com>
2023-08-04 02:57:29 -0400

    Finished exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const bookNames = array.map(book => book.title);
+    return bookNames;
 };
 
 // Do not edit below this line
